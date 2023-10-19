30c9eaa58d11bf1f3e4721c93dd9a9aa580ba578
Twinkll Sisodia <tsisodia@redhat.com>
2023-10-19 16:00:26 -0400

    Application to interact with openAI

---
+++ app.py
@@ -0,0 +1,31 @@
+import openai
+import gradio as gr
+
+# Set your OpenAI API key
+openai.api_key = ''
+
+def generate_response(prompt, max_tokens=50):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=max_tokens
+    )
+    return response.choices[0].text
+
+def generate_response_with_gradio(prompt, max_tokens=50):
+    response = generate_response(prompt, max_tokens)
+    return response
+
+iface = gr.Interface(
+    fn=generate_response_with_gradio,
+    inputs="text",
+    outputs="text",
+    layout="vertical",
+    title="Few-Shot Prompting",
+    description="Enter a text prompt.",
+    capture_session=True
+)
+
+if __name__ == '__main__':
+    iface.launch()
+
