9b457481cd1d7266371c5b02dbdc3e287ecfbcf6
Twinkll Sisodia <tsisodia@redhat.com>
2023-10-26 16:55:54 -0400

    Add chatbot app, along with flask app to get the feedback rating and redis app to check if the data got persisted

---
+++ redis-check.py
@@ -0,0 +1,18 @@
+import redis
+
+# Connect to the Redis server
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+# Specify the key where ratings are stored
+ratings_key = 'ratings'
+
+# Use the LRANGE command to retrieve all ratings
+ratings = redis_client.lrange(ratings_key, 0, -1)
+
+# Check if ratings were retrieved
+if ratings:
+    print("Stored Ratings:")
+    for rating in ratings:
+        print(rating.decode('utf-8'))  # Decode bytes to a string
+else:
+    print("No ratings found in Redis.")
